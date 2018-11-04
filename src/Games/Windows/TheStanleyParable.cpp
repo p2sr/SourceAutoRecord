2914ccbda8753673444203a2308ceb3414b686bb
NeKz <nekzor@users.noreply.github.com>
2018-11-04 19:45:11 +0100

    Add more TODOs

--- src/Games/Windows/TheStanleyParable.cpp
+++ src/Games/Windows/TheStanleyParable.cpp
@@ -20,6 +20,20 @@ void TheStanleyParable::LoadOffsets()
     interval_per_tick = 73; // CClientState::ProcessTick
     HostState_OnClientConnected = 695; // CClientState::SetSignonState
 
+    // server.dll
+    m_iClassName = 104; // CBaseEntity (TODO)
+    m_iName = 216; // CBaseEntity (TODO)
+    m_vecAbsOrigin2 = 468; // CBaseEntity (TODO)
+    m_angAbsRotation2 = 480; // CBaseEntity (TODO)
+    m_vecVelocity3 = 492; // CBaseEntity (TODO)
+    m_iEFlags = 208; // CBaseEntity (TODO)
+    m_flMaxspeed = 3728; // CBaseEntity (TODO)
+    m_flGravity = 792; // CBaseEntity (TODO)
+    m_vecViewOffset = 748; // CBaseEntity (TODO)
+    NUM_ENT_ENTRIES = 4096; // CBaseEntityList::CBaseEntityList (TODO)
+    GetIServerEntity = 2; // CServerTools (TODO)
+    m_EntPtrArray = 48; // CServerTools::GetIServerEntity (TODO)
+
     // client.dll
 
     IN_ActivateMouse = 15; // CHLClient
