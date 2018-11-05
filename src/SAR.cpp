abef22b9ae724398e5bd858af97417f71e3540d2
Blenderiste09 <flodege@gmail.com>
2018-11-05 19:25:41 +0100

    Added a LookAt function

--- src/SAR.cpp
+++ src/SAR.cpp
@@ -25,6 +25,7 @@
 #include "Features/Summary.hpp"
 #include "Features/Tas/CommandQueuer.hpp"
 #include "Features/Tas/ReplaySystem.hpp"
+#include "Features/Tas/AutoAiming.hpp"
 #include "Features/Teleporter.hpp"
 #include "Features/Timer/Timer.hpp"
 #include "Features/WorkshopList.hpp"
@@ -72,6 +73,7 @@ bool SAR::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerF
             this->features->AddFeature<Stats>(&stats);
             this->features->AddFeature<CommandQueuer>(&tasQueuer);
             this->features->AddFeature<ReplaySystem>(&tasReplaySystem);
+			this->features->AddFeature<AutoAiming>(&autoAiming);
             this->features->AddFeature<Timer>(&timer);
             this->features->AddFeature<EntityInspector>(&inspector);
 
