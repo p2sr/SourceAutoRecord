654f86bed5dfbf159490f05cb24834e872fc126a
NeKz <nekzor@users.noreply.github.com>
2018-11-07 00:21:11 +0100

    Move replay class and structs to own file

--- src/Features/Tas/ReplaySystem.cpp
+++ src/Features/Tas/ReplaySystem.cpp
@@ -2,59 +2,13 @@
 
 #include <fstream>
 
+#include "Replay.hpp"
+
 #include "Modules/Console.hpp"
 #include "Modules/Engine.hpp"
 
 #include "Command.hpp"
 #include "Variable.hpp"
-
-Replay::Replay()
-    : views(1)
-    , playIndex(0)
-{
-}
-void Replay::Reset()
-{
-    this->playIndex = 0;
-}
-bool Replay::Ended()
-{
-    return this->playIndex + 1 >= (int)this->views[0].frames.size();
-}
-void Replay::Resize()
-{
-    for (auto& view : this->views) {
-        view.frames.resize(this->playIndex);
-    }
-}
-void Replay::Record(CUserCmd* cmd, int slot)
-{
-    this->views[slot].frames.push_back(ReplayFrame{
-        cmd->viewangles,
-        cmd->forwardmove,
-        cmd->sidemove,
-        cmd->upmove,
-        cmd->buttons,
-        cmd->impulse,
-        cmd->mousedx,
-        cmd->mousedy });
-}
-void Replay::Play(CUserCmd* cmd, int slot)
-{
-    if (this->Ended())
-        return;
-
-    auto frame = this->views[slot].frames[this->playIndex++];
-
-    cmd->viewangles = frame.viewangles;
-    cmd->forwardmove = frame.forwardmove;
-    cmd->sidemove = frame.sidemove;
-    cmd->upmove = frame.upmove;
-    cmd->buttons = frame.buttons;
-    cmd->impulse = frame.impulse;
-    cmd->mousedx = frame.mousedx;
-    cmd->mousedy = frame.mousedy;
-}
 
 Variable sar_replay_autorecord("sar_replay_autorecord", "0", "Starts recording inputs on first frame after a load.\n");
 Variable sar_replay_autoplay("sar_replay_autoplay", "0", "Starts playing back recorded inputs on first frame after a load.\n");
