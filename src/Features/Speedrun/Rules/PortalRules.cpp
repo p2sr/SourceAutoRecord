9c5441044b60cd211ca474a2bc518c31427bc737
NeKzor <nekzor@users.noreply.github.com>
2019-01-05 19:15:04 +0100

    Fix Portal rules on windows

--- src/Features/Speedrun/Rules/PortalRules.cpp
+++ src/Features/Speedrun/Rules/PortalRules.cpp
@@ -5,29 +5,37 @@
 #include "Features/Speedrun/TimerCategory.hpp"
 #include "Features/Speedrun/TimerRule.hpp"
 
+#include "Modules/Console.hpp"
+#include "Modules/Engine.hpp"
+
 #include "Utils/SDK.hpp"
 
 #ifdef _WIN32
-#define Offset_m_state 868
-#define Offset_m_iDisabled 822
+#define testchmb_a_00 "testchmb_a_00"
+#define escape_02 "escape_02"
+#define Offset_m_state 872
+#define Offset_m_iDisabled 828
 #else
+#define testchmb_a_00 "maps/testchmb_a_00.bsp"
+#define escape_02 "maps/escape_02.bsp"
 #define Offset_m_state 892
 #define Offset_m_iDisabled 848
 #endif
 
-SAR_RULE3(waking_up, "maps/testchmb_a_00.bsp", "blackout_viewcontroller", SearchMode::Names)
+SAR_RULE3(waking_up, testchmb_a_00, "blackout_viewcontroller", SearchMode::Names)
 {
     // CTriggerCamera aka point_viewcontrol
     auto m_state = reinterpret_cast<int*>((uintptr_t)entity + Offset_m_state);
 
-    if (*m_state == USE_OFF) {
+    // Give it some time to get enabled...
+    if (engine->GetSessionTick() > 10 && *m_state == USE_OFF) {
         return TimerAction::Start;
     }
 
     return TimerAction::DoNothing;
 }
 
-SAR_RULE3(glados_beaten, "maps/escape_02.bsp", "player_clip_glados", SearchMode::Names)
+SAR_RULE3(glados_beaten, escape_02, "player_clip_glados", SearchMode::Names)
 {
     // CFuncBrush aka func_brush
     auto m_iDisabled = reinterpret_cast<int*>((uintptr_t)entity + Offset_m_iDisabled);
