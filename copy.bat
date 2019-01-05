050abeff82731f5e97bc2f6853461d05470f8e9b
NeKzor <nekzor@users.noreply.github.com>
2019-01-05 16:13:35 +0100

    Fix copy.bat script (again)

--- copy.bat
+++ copy.bat
@@ -1,10 +1,11 @@
 ECHO !!! YOU FORGOT TO EDIT copy.bat !!!
 
-SET "src=..\bin\sar.dll"
+SET "binary=sar.dll"
+SET "src=..\bin\%binary%"
 SET "dest=C:\Program Files (x86)\Steam\SteamApps\common"
 
-REM copy /Y "%src%" "%dest%\Portal 2\$(TargetName).dll"
-REM copy /Y "%src%" "%dest%\Portal\$(TargetName).dll"
-REM copy /Y "%src%" "%dest%\Half-Life 2\$(TargetName).dll"
-REM copy /Y "%src%" "%dest%\The Stanley Parable\$(TargetName).dll"
-REM copy /Y "%src%" "%dest%\The Beginners Guide\$(TargetName).dll"
+REM copy /Y "%src%" "%dest%\Portal 2\%binary%"
+REM copy /Y "%src%" "%dest%\Portal\%binary%"
+REM copy /Y "%src%" "%dest%\Half-Life 2\%binary%"
+REM copy /Y "%src%" "%dest%\The Stanley Parable\%binary%"
+REM copy /Y "%src%" "%dest%\The Beginners Guide\%binary%"
