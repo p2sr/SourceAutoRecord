7e9d5122d704d619e5ffabc0448edf6ef23539da
NeKz <nekzor@users.noreply.github.com>
2019-01-20 00:40:20 +0100

    Move math functions to Math.cpp

--- src/Utils/Math.hpp
+++ src/Utils/Math.hpp
@@ -1,7 +1,4 @@
 #pragma once
-#include <cmath>
-#include <float.h>
-
 #include "SDK.hpp"
 
 #define M_PI 3.14159265358979323846
@@ -11,111 +8,14 @@
 
 namespace Math {
 
-inline void SinCos(float radians, float* sine, float* cosine)
-{
-#ifdef _WIN32
-    _asm {
-        fld DWORD PTR[radians]
-        fsincos
+void SinCos(float radians, float* sine, float* cosine);
 
-        mov edx, DWORD PTR[cosine]
-        mov eax, DWORD PTR[sine]
+void AngleVectors(const QAngle& angles, Vector* forward);
+float AngleNormalize(float angle);
 
-        fstp DWORD PTR[edx]
-        fstp DWORD PTR[eax]
-    }
-#else
-    register double __cosr, __sinr;
-    __asm("fsincos"
-          : "=t"(__cosr), "=u"(__sinr)
-          : "0"(radians));
-
-    *sine = __sinr;
-    *cosine = __cosr;
-#endif
-}
-
-inline void AngleVectors(const QAngle& angles, Vector* forward)
-{
-    float sp, sy, cp, cy;
-
-    SinCos(DEG2RAD(angles.y), &sy, &cy);
-    SinCos(DEG2RAD(angles.x), &sp, &cp);
-
-    forward->x = cp * cy;
-    forward->y = cp * sy;
-    forward->z = -sp;
-}
-
-inline float AngleNormalize(float angle)
-{
-    angle = fmodf(angle, 360.0f);
-    if (angle > 180) {
-        angle -= 360;
-    }
-    if (angle < -180) {
-        angle += 360;
-    }
-    return angle;
-}
-
-inline float VectorNormalize(Vector& vec)
-{
-    auto radius = sqrtf(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
-    auto iradius = 1.f / (radius + FLT_EPSILON);
-
-    vec.x *= iradius;
-    vec.y *= iradius;
-    vec.z *= iradius;
-
-    return radius;
-}
-
-inline void VectorAdd(const Vector& a, const Vector& b, Vector& c)
-{
-    c.x = a.x + b.x;
-    c.y = a.y + b.y;
-    c.z = a.z + b.z;
-}
-
-inline void AngleVectors(const QAngle& angles, Vector* forward, Vector* right, Vector* up)
-{
-    float sr, sp, sy, cr, cp, cy;
-
-    SinCos(DEG2RAD(angles.x), &sy, &cy);
-    SinCos(DEG2RAD(angles.y), &sp, &cp);
-    SinCos(DEG2RAD(angles.z), &sr, &cr);
-
-    if (forward) {
-        forward->x = cp * cy;
-        forward->y = cp * sy;
-        forward->z = -sp;
-    }
-
-    if (right) {
-        right->x = (-1 * sr * sp * cy + -1 * cr * -sy);
-        right->y = (-1 * sr * sp * sy + -1 * cr * cy);
-        right->z = -1 * sr * cp;
-    }
-
-    if (up) {
-        up->x = (cr * sp * cy + -sr * -sy);
-        up->y = (cr * sp * sy + -sr * cy);
-        up->z = cr * cp;
-    }
-}
-
-inline void VectorScale(Vector const& src, float b, Vector& dst)
-{
-    dst.x = src.x * b;
-    dst.y = src.y * b;
-    dst.z = src.z * b;
-}
-
-inline void VectorCopy(const Vector& src, Vector& dst)
-{
-    dst.x = src.x;
-    dst.y = src.y;
-    dst.z = src.z;
-}
+float VectorNormalize(Vector& vec);
+void VectorAdd(const Vector& a, const Vector& b, Vector& c);
+void AngleVectors(const QAngle& angles, Vector* forward, Vector* right, Vector* up);
+void VectorScale(Vector const& src, float b, Vector& dst);
+void VectorCopy(const Vector& src, Vector& dst);
 }
