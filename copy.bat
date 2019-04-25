bd2828bd2b1ff82663a2d006af1cfacd5782115c
NeKz <nekzor@users.noreply.github.com>
2019-04-25 23:14:04 +0200

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,5 +1,12 @@
+ECHO !!! YOU FORGOT TO EDIT copy.bat !!!
+
 SET "binary=sar.dll"
 SET "src=..\bin\%binary%"
-SET "dest=E:\Steam\steamapps\common"
+SET "dest=C:\Program Files (x86)\Steam\SteamApps\common"
 
-copy /Y "%src%" "%dest%\Portal 2\%binary%"
+REM copy /Y "%src%" "%dest%\Portal 2\%binary%"
+REM copy /Y "%src%" "%dest%\Portal\%binary%"
+REM copy /Y "%src%" "%dest%\Half-Life 2\%binary%"
+REM copy /Y "%src%" "%dest%\The Stanley Parable\%binary%"
+REM copy /Y "%src%" "%dest%\The Beginners Guide\%binary%"
+REM copy /Y "%src%" "%dest%\INFRA\%binary%"
