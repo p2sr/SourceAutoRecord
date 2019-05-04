942eb130ba786047c42911f7f9ecd330306a328a
NeKz <nekzor@users.noreply.github.com>
2019-05-04 17:10:10 +0200

    Add Super 8 category for Portal 2

--- src/Features/Speedrun/Rules/Portal2Rules.cpp
+++ src/Features/Speedrun/Rules/Portal2Rules.cpp
@@ -12,11 +12,13 @@
 #define Offset_m_bDisabled 980
 #define Offset_m_bPlayerStateA 960
 #define Offset_m_bPlayerStateB 961
+#define Offset_m_flFOV 1736
 #else
 #define Offset_m_iTouchingPortalCount 1148
 #define Offset_m_bDisabled 1004
 #define Offset_m_bPlayerStateA 984
 #define Offset_m_bPlayerStateB 985
+#define Offset_m_flFOV 1760
 #endif
 
 SAR_RULE(view_change, "sp_a1_intro1", "player", "CBasePlayer", m_hViewEntity, SearchMode::Classes)
@@ -79,5 +81,29 @@ SAR_RULE3(gate_opens, "mp_coop_paint_crazy_box", "coopman_airlock_success", Sear
     return TimerAction::DoNothing;
 }
 
-SAR_CATEGORY(Portal2, RTA, _Rules({ &view_change, &moon_shot }));
+SAR_RULE(gain_control, "e1912", "player", "CBasePlayer", m_hVehicle, SearchMode::Classes)
+{
+    // Wait some ticks till we get into the vehicle
+    if (engine->GetSessionTick() > 13 && *m_hVehicle == -1) {
+        return TimerAction::Start;
+    }
+
+    return TimerAction::DoNothing;
+}
+
+SAR_RULE3(vehicle_lock, "e1912", "crash-vehicle_outro", SearchMode::Names)
+{
+    // CPropVehicleChoreoGeneric aka prop_vehicle_choreo_generic
+    // m_savedVehicleView.flFOV
+    auto flFOV = reinterpret_cast<float*>((uintptr_t)entity + Offset_m_flFOV);
+
+    if (*flFOV != 0) {
+        return TimerAction::End;
+    }
+
+    return TimerAction::DoNothing;
+}
+
+SAR_CATEGORY(Portal2, RTA,     _Rules({ &view_change, &moon_shot }));
 SAR_CATEGORY(Portal2, CoopRTA, _Rules({ &players_teleport, &players_taunt, &gate_opens }));
+SAR_CATEGORY(Portal2, Super8,  _Rules({ &gain_control, &vehicle_lock }));
