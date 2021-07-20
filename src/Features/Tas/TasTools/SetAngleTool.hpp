1031b5d086f04c520fe4e8b284524ccfc0c49f13
mlugg <mlugg@mlugg.co.uk>
2021-07-20 14:42:41 +0100

    feat: add setang TAS tool

---
+++ src/Features/Tas/TasTools/SetAngleTool.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "Features/Tas/TasPlayer.hpp"
+#include "Features/Tas/TasTool.hpp"
+
+class SetAngleTool : public TasTool {
+public:
+    SetAngleTool()
+        : TasTool("setang")
+    { }
+    virtual SetAngleTool* GetTool();
+    virtual std::shared_ptr<TasToolParams> ParseParams(std::vector<std::string>);
+    virtual void Apply(TasFramebulk& fb, const TasPlayerInfo& pInfo);
+    virtual void Reset();
+};
