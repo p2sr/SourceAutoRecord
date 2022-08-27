665540b9f8174e9e4421a10aa9e5c0327fc78d09
Jesse <69196954+ThisAMJ@users.noreply.github.com>
2022-08-27 13:15:48 +1000

    feat: sar_on_xxx_list

--- src/Features/ConfigPlus.cpp
+++ src/Features/ConfigPlus.cpp
@@ -667,23 +667,29 @@ static Condition *ParseCondition(std::queue<Token> toks) {
 
 // }}}
 
-#define MK_SAR_ON(name, when, immediately)                                                                                           \
-	static std::vector<std::string> _g_execs_##name;                                                                                    \
-	CON_COMMAND_F(sar_on_##name, "sar_on_" #name " <command> [args]... - registers a command to be run " when "\n", FCVAR_DONTRECORD) { \
-		if (args.ArgC() < 2) {                                                                                                             \
-			return console->Print(sar_on_##name.ThisPtr()->m_pszHelpString);                                                                  \
-		}                                                                                                                                  \
-		const char *cmd = args.ArgC() == 2 ? args[1] : args.m_pArgSBuffer + args.m_nArgv0Size;                                             \
-		_g_execs_##name.push_back(std::string(cmd));                                                                                       \
-	}                                                                                                                                   \
+#define MK_SAR_ON(name, when, immediately)                                                                                                \
+	static std::vector<std::string> _g_execs_##name;                                                                                         \
+	CON_COMMAND_F(sar_on_##name, "sar_on_" #name " <command> [args]... - registers a command to be run " when "\n", FCVAR_DONTRECORD) {      \
+		if (args.ArgC() < 2) {                                                                                                                  \
+			return console->Print(sar_on_##name.ThisPtr()->m_pszHelpString);                                                                       \
+		}                                                                                                                                       \
+		const char *cmd = args.ArgC() == 2 ? args[1] : args.m_pArgSBuffer + args.m_nArgv0Size;                                                  \
+		_g_execs_##name.push_back(std::string(cmd));                                                                                            \
+	}                                                                                                                                        \
 	CON_COMMAND_F(sar_on_##name##_clear, "sar_on_" #name "_clear - clears commands registered on event \"" #name "\"\n", FCVAR_DONTRECORD) { \
-		console->Print("Cleared %d commands from event \"" #name "\"\n", _g_execs_##name.size());                                  \
-		_g_execs_##name.clear();                                                                                       \
-	}                                                                                                                                   \
-	static void _runExecs_##name() {                                                                                                    \
-		for (auto cmd : _g_execs_##name) {                                                                                                 \
-			engine->ExecuteCommand(cmd.c_str(), immediately);                                                                                 \
-		}                                                                                                                                  \
+		console->Print("Cleared %d commands from event \"" #name "\"\n", _g_execs_##name.size());                                               \
+		_g_execs_##name.clear();                                                                                                                \
+	}                                                                                                                                        \
+	CON_COMMAND_F(sar_on_##name##_list, "sar_on" #name "_list - lists commands registered on event \"" #name "\"\n", FCVAR_DONTRECORD) {     \
+		console->Print("%d commands on event \"" #name "\"\n", _g_execs_##name.size());                                                         \
+		for (auto cmd : _g_execs_##name) {                                                                                                      \
+			console->Print("%s\n", cmd.c_str());                                                                                                   \
+		}                                                                                                                                       \
+	}                                                                                                                                        \
+	static void _runExecs_##name() {                                                                                                         \
+		for (auto cmd : _g_execs_##name) {                                                                                                      \
+			engine->ExecuteCommand(cmd.c_str(), immediately);                                                                                      \
+		}                                                                                                                                       \
 	}
 
 #define RUN_EXECS(x) _runExecs_##x()
