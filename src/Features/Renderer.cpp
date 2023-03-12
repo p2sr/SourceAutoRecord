a433baf57a4e7090ca194eb2112cd58597586b9b
Krzyhau <krzyht@gmail.com>
2023-03-12 16:17:32 +0100

    feat: add basic discord rich presence
    might not work on linux

--- src/Features/Renderer.cpp
+++ src/Features/Renderer.cpp
@@ -1170,6 +1170,10 @@ void Renderer::Cleanup() {
 	Command::Unhook("endmovie", endmovie_origCbk);
 }
 
+bool Renderer::IsRunning() {
+	return g_render.isRendering.load();
+}
+
 // }}}
 
 // Commands {{{
--- src/Features/Renderer.hpp
+++ src/Features/Renderer.hpp
@@ -4,6 +4,7 @@ namespace Renderer {
 	void Frame();
 	void Init(void **videomode);
 	void Cleanup();
+	bool IsRunning();
 	extern int segmentEndTick;
 	extern bool isDemoLoading;
 };  // namespace Renderer
