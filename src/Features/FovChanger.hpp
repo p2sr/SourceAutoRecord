936b1f6bd709808d8ff81a3171782bafe4b25706
Zyntex <komocsinlaci@gmail.com>
2023-10-28 14:26:43 +0200

    feat: force viewmodel fov

--- src/Features/FovChanger.hpp
+++ src/Features/FovChanger.hpp
@@ -5,10 +5,12 @@
 class FovChanger : public Feature {
 private:
 	int defaultFov;
+	int viewmodelFov;
 
 public:
 	FovChanger();
 	void SetFov(const int fov);
+	void SetViewmodelFov(const int fov);
 	void Force();
 
 	bool needToUpdate = false;
