574e005eb28b52ca0c79db52cf5027d5a3572633
NeKz <nekzor@users.noreply.github.com>
2023-11-04 03:57:27 +0100

    refactor: move code to AutoSubmitMod namespace

--- src/Features/AutoSubmitMod.hpp
+++ src/Features/AutoSubmitMod.hpp
@@ -9,5 +9,8 @@ namespace AutoSubmitMod {
 	void LoadApiKey(bool output_nonexist);
 	void FinishRun(float final_time, const char *demopath, std::optional<std::string> rename_if_pb, std::optional<std::string> replay_append_if_pb);
 	std::optional<std::string> GetMapId(std::string map_name);
+	void Search(const char *map);
 	json11::Json::array GetTopScores(std::string &map_id);
+	bool IsQuerying();
+	const std::vector<json11::Json>& GetTimes();
 };  // namespace AutoSubmitMod
