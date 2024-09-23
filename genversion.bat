f6e0cf4e66039ddde20e4b3e8b4638589713e557
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2024-09-23 12:10:43 +1000

    refactor: fix definition
    on windows the echo command eats the number. this
    didn't have any functional impact, but it could
    be confusing.

--- genversion.bat
+++ genversion.bat
@@ -1,7 +1,7 @@
 for /f "tokens=* usebackq" %%x in (`git describe --tags`) do (set version=%%x)
 if "%RELEASE_BUILD%"=="" (
     echo #define SAR_VERSION "%version%-canary">Version.hpp
-    echo #define SAR_DEV_BUILD 1>>Version.hpp
+    echo #define SAR_DEV_BUILD 1 >>Version.hpp
 ) else (
     echo #define SAR_VERSION "%version%">Version.hpp
 )
