8df750b9fccac0c45f7acd10362be4e102b560f0
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2024-09-24 13:11:54 +1000

    fix: twtm linux

--- src/Modules/Matchmaking.cpp
+++ src/Modules/Matchmaking.cpp
@@ -16,7 +16,11 @@ bool Matchmaking::Init() {
 #ifdef _WIN32
 		Matchmaking::UpdateLeaderboardData = (decltype(Matchmaking::UpdateLeaderboardData))Memory::Scan(matchmaking->Name(), "55 8B EC 83 EC 08 53 8B D9 8B 03 8B 50 08");
 #else
-		Matchmaking::UpdateLeaderboardData = (decltype(Matchmaking::UpdateLeaderboardData))Memory::Scan(matchmaking->Name(), "55 89 E5 57 56 53 83 EC 2C 8B 45 08 8B 5D 0C");
+		if (sar.game->Is(SourceGame_PortalReloaded) || sar.game->Is(SourceGame_PortalStoriesMel)) {
+			Matchmaking::UpdateLeaderboardData = (decltype(Matchmaking::UpdateLeaderboardData))Memory::Scan(matchmaking->Name(), "55 89 E5 57 56 53 83 EC 2C 8B 45 08 8B 5D 0C");
+		} else {
+			Matchmaking::UpdateLeaderboardData = (decltype(Matchmaking::UpdateLeaderboardData))Memory::Scan(matchmaking->Name(), "55 89 E5 57 56 53 83 EC 3C 8B 55 ? 8B 02 89 14 24 FF 50 ? C7 04 24");
+		}
 #endif
 
 		g_UpdateLeaderboardDataHook.SetFunc(Matchmaking::UpdateLeaderboardData);
