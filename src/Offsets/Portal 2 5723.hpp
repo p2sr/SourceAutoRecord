d407de8d69c732517ce97615a809525fa6e91cd3
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2024-11-26 20:43:36 +1100

    fix: include function sigs

--- src/Offsets/Portal 2 5723.hpp
+++ src/Offsets/Portal 2 5723.hpp
@@ -23,7 +23,9 @@ OFFSET_LINUX(snd_vol, 80)
 SIGSCAN_LINUX(PathMatch, "55 89 E5 57 56 53 83 EC ? 0F B6 45 ? 80 3D")
 
 // Client
+SIGSCAN_LINUX(DrawPortal, "55 89 E5 83 EC 58 A1 ? ? ? ? 89 5D ? 89 75 ? 8B 5D ? 89 7D ? 8B 75 ? 8B 10")
 SIGSCAN_LINUX(DrawPortalSpBranch, "0F 85 ? ? ? ? 0F B6 83 ? ? ? ? 8B 3C 85 ? ? ? ? A1 ? ? ? ? 89 04 24 E8 ? ? ? ? 84 C0 74")
 OFFSET_LINUX(DrawPortalSpBranchOff, 0x15)
+SIGSCAN_LINUX(DrawPortalGhost, "55 89 E5 57 56 53 83 EC 5C A1 ? ? ? ? 8B 40")
 SIGSCAN_LINUX(DrawPortalGhostSpBranch, "0F 84 ? ? ? ? FF 90 ? ? ? ? 80 BB ? ? ? ? 01")
 SIGSCAN_LINUX(GetChapterProgress, "55 89 E5 57 56 53 83 EC 2C 8B 7D 08 E8 ? ? ? ? 8B 10 C7")
