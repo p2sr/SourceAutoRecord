5a6659efc352398c29d57de5dd9b7913389b6217
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2025-01-31 17:35:17 +1100

    fix: crash on launch
    im going fucking mental trying to fix +bhop

--- src/Offsets/Beginners Guide 6167.hpp
+++ src/Offsets/Beginners Guide 6167.hpp
@@ -28,6 +28,7 @@ OFFSET_DEFAULT(m_bWaitEnabled, 8264, 8264)
 // CMaterialSystem
 OFFSET_WINDOWS(CreateMaterial, 80)
 // OFFSET_WINDOWS(FindMaterial, 81)
+OFFSET_WINDOWS(CreateProceduralTexture, 90)
 // OFFSET_WINDOWS(GetRenderContext, 112)
 
 // IVideoMode
