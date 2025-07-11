935dd3acb8de248b88b545c28a9f12c3f164eb7f
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2025-07-12 09:47:25 +1000

    fix: INFRA memory leak but better
    this is being pretty trusting of the engine, hoping that the second environment will ALWAYS be the one that is used. but from testing it seems pretty stable!

--- src/Modules/VPhysics.cpp
+++ src/Modules/VPhysics.cpp
@@ -9,17 +9,14 @@
 // Kinda a silly way to keep track of this, but it works
 // and I'm lazy
 static std::vector<int *> vphysEnvironments;
-static std::vector<int *> vphysEnvironmentsLast;
+static bool lastWasFast = false;
 
 // INFRA memory leak
 // The game has a "fast load" feature that skips a bunch of stuff, including
 // destroying the old physics environment. This leaks ~4MB (one env) per load.
-// Slow loads destroy the two environments that are in use, and create a new
-// two. We detect this and destroy the old environments that are no longer in
-// use. There are other leaks, but this is the most significant.
+// There are other leaks, but this is the most significant.
 ON_EVENT(SESSION_START) {
 	if (!sar.game->Is(SourceGame_INFRA)) return;
-	vphysEnvironmentsLast = vphysEnvironments;
 	vphysEnvironments = {};
 	for (int i = 0; ; ++i) {
 		int *env = vphysics->GetActivePhysicsEnvironmentByIndex(i);
@@ -27,36 +24,20 @@ ON_EVENT(SESSION_START) {
 		vphysEnvironments.push_back(env);
 	}
 
-	// console->Print("%d active physics environment(s)\n", (int)vphysEnvironments.size());
-	// int i = 0;
-	// for (const auto &env : vphysEnvironmentsLast) {
-	// 	if (std::find(vphysEnvironments.begin(), vphysEnvironments.end(), env) == vphysEnvironments.end()) {
-	// 		console->ColorMsg(Color(255,96,64), "- %d %p\n", i, env);
-	// 	}
-	// 	i++;
-	// }
-	// i = 0;
-	// for (const auto &env : vphysEnvironments) {
-	// 	if (std::find(vphysEnvironmentsLast.begin(), vphysEnvironmentsLast.end(), env) == vphysEnvironmentsLast.end()) {
-	// 		console->ColorMsg(Color(96,255,64), "+ %d %p\n", i, env);
-	// 	} else {
-	// 		console->Print("  %d %p\n", i, env);
-	// 	}
-	// 	i++;
-	// }
-
-	int newEnvs = 0;
-	for (const auto &env : vphysEnvironments) {
-		if (std::find(vphysEnvironmentsLast.begin(), vphysEnvironmentsLast.end(), env) == vphysEnvironmentsLast.end()) {
-			newEnvs++;
-		}
-	}
-	if (newEnvs == 2) {
-		for (const auto &env : vphysEnvironments) {
-			if (std::find(vphysEnvironmentsLast.begin(), vphysEnvironmentsLast.end(), env) != vphysEnvironmentsLast.end()) {
-				vphysics->DestroyPhysicsEnvironment(env);
-			}
-		}
+	switch (vphysEnvironments.size()) {
+		default:
+			lastWasFast = false;
+			break;
+		case 3:
+			// Fast load, destroy one unused environment
+			// The first env is unused, and the second one persists through
+			// fast load(s). That means on the first fast load we'll destroy
+			// the first environment, and subsequent fast loads we'll destroy
+			// the second environment. (since it's now using the first one)
+			int *env = vphysEnvironments[lastWasFast ? 1 : 0];
+			vphysics->DestroyPhysicsEnvironment(env);
+			lastWasFast = true;
+			break;
 	}
 }
 
